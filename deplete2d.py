047401820fcf7ff876bfcf27da690a9512957af1
Richard Brown <rgbrown@gmail.com>
2014-11-11 17:15:13 +1300

    extracted deplete2d.py from notebook

--- deplete2d.py
+++ deplete2d.py
@@ -1,138 +1,173 @@
-# Main simulation script. Let's just whack it all in and see what happens
-
-# New and improved version, using KDTree to speedup the search process
+# coding: utf-8
 import numpy as np
-import scipy.spatial
 import matplotlib.pyplot as plt
+import scipy.spatial
 
-class sim2d:
+# Prey Depletion numerical experiments
+# ====================================
+# 
+# We want to investigate how accumulated prey depletion slows the rate of population depletion when compared with a well-mixed model.
 
-    def __init__(self, mu=100, r=0.01, v=1, plot=False):
-        self.mu = mu
-        self.r = r
-        self.v = v
-        self.plot = plot
+# deplete2d class
+# ---------------
+# 
+# Below is a class that sets up an environment with a random number of prey items of a specified Poisson density, and a single predator. The predator has a given perceptive radius, and constant speed, and individual vector steps can be specified.
 
-        # Generate prey
-        self.n = np.random.poisson(self.mu)
-        self.x_prey = np.random.rand(self.n, 2)
-        self.x_prey_orig = self.x_prey.copy()
+def init_prey(n):
+    """
+    Initialize prey using a structured array
+    """
+    
+    prey = np.zeros(n, dtype='uint32, 2float64, uint8')
+    prey.dtype.names = ('index', 'position', 'alive')
+    prey['index'] = range(n)
+    prey['position'] = np.random.rand(n, 2)
+    prey['alive'] = np.ones(n)
+    return prey
+    
+def init_output_array(n):
+    output = np.zeros(n, dtype='float64, uint32')
+    output.dtype.names = ('time', 'index')
+    return output
 
-        # Initialise arrays
-        self.t = 0
-        self.n_eaten = 0
-        self.popn = self.n
-        self.t_eat = np.zeros(self.n + 1)
-        self.u = np.zeros(self.n + 1)
-        self.u[0] = self.n
-        self.i_eat = np.zeros(self.n)
-        self.alive = np.array([True for i in range(self.n)])
-        self.pointer = 1
-
+class deplete2d:
+    def __init__(self, density=100, speed=1., radius=0.01):
+        # The actual number of prey is Poisson distributed
+        self.n_prey = scipy.random.poisson(density)
+        self.population = self.n_prey
+        self.output = init_output_array(self.n_prey)
+        self.outp = 0
+        self.time = 0.
+        
+        # Initialise prey, and make a copy to work on, and compute KDTree
+        self.prey_original = init_prey(self.n_prey)
+        self.prey = self.prey_original[self.prey_original['alive'].nonzero()]
+        self.tree = scipy.spatial.cKDTree(self.prey['position'])
+        
+        self.speed = speed
+        self.radius = radius
         self.x_pred = np.array([0.5, 0.5])
-        self.tree = scipy.spatial.cKDTree(self.x_prey)
-        if self.plot:
-            self.fig = plt.figure()
-            self.ax = self.fig.add_subplot(111)
-            self.markers_alive = self.ax.plot(self.x_prey[self.alive,0], 
-                    self.x_prey[self.alive,1], 'bo')
-            self.marker_pred = self.ax.plot(self.x_pred[0], self.x_pred[1], 'ko')
-            self.fig.canvas.draw()
-
+        
+    def update_domain(self, centre=[0.5,0.5]):
+        """
+        translate the domain (mod 1) so that centre is the new centre,
+        copy dead prey information to original block,
+        create new working prey set,
+        update KD Tree
+        """
+        centre = np.array(centre)
+        assert(centre.size == 2)
+        
+        # First, copy dead dude information back to original prey block
+        i_dead = self.prey['alive'] == 0
+        self.prey_original['alive'][self.prey[i_dead]['index']] = 0
+        
+        # Remove dead prey items
+        self.prey = self.prey[~i_dead]
+        
+        # Recenter and recompute tree
+        self.prey['position'] = np.remainder(self.prey['position'] + 
+                                          np.array([0.5, 0.5]) - centre, 1.)
+               
+        if self.prey.size != 0:
+            self.tree = scipy.spatial.cKDTree(self.prey['position'])
+        
     def step(self, u):
-        # If step would take us outside of the boundary, recenter and
-        # recompute KDTree. We recenter so that the predator ends up at (0.5, 0.5) at the end of his step
-        if ((self.x_pred + u < self.r) | (self.x_pred + u > 1 - self.r)).any():
-            dx = np.array([0.5, 0.5]) - self.x_pred - u
-            self.x_prey = np.mod(self.x_prey + dx, 1)
-            self.x_pred = self.x_pred + dx
-            self.tree = scipy.spatial.cKDTree(self.x_prey)
+        if self.population == 0:
+            return
+        u = np.array(u)
+        assert(u.size == 2)
+        
+        # Check whether domain update is required
+        if ((self.x_pred + u < self.radius) |
+            (self.x_pred + u > 1 - self.radius)).any():
+            self.update_domain(self.x_pred + u)
+            self.x_pred = np.array([0.5, 0.5]) - u
             
-            
-        # find candidate points for closeness from KDTree - using radius of l + r from start point. is_candidate will be a Boolean array of points
-        # that are in the neighbourhood and are alive
-        l = np.linalg.norm(u)
-        is_candidate = np.zeros(self.n, dtype=bool)
-        is_candidate[self.tree.query_ball_point(self.x_pred, l+self.r)] = True
-        is_candidate = (is_candidate & self.alive).nonzero()[0]
+        # We may now assume that our step will stay in bounds
+        step_length = np.hypot(u[0], u[1])
         
-        z = self.x_prey[is_candidate]
-        idx, d = self.find_prey(z, u)
-        n_eaten = idx[0].size
-        self.popn = self.popn - n_eaten
-        self.alive[is_candidate[idx]] = False
-        self.x_pred = self.x_pred + u
+        # Query KDTree for nearby points
+        i_nearby = np.array(self.tree.query_ball_point(self.x_pred, step_length + self.radius))
         
-        # Update the arrays
-        # print d
-        self.t_eat[self.pointer:(self.pointer + n_eaten)] = d / self.v + self.t
-        self.u[self.pointer:(self.pointer + n_eaten)] = self.u[self.pointer-1] + np.arange(-1,-n_eaten-1,-1)
-        self.pointer = self.pointer + n_eaten
-        self.t = self.t + l / self.v
+        # Now, create a smaller copy to work on, with only nearby (and alive) dudes
+        if i_nearby.size != 0:
+            prey_local = self.prey[i_nearby]
+            i_keep = prey_local['alive'] > 0
+            i_nearby = i_nearby[i_keep]
+            prey_local = prey_local[i_keep]
+      
+            # Find the indices (into prey_local) of the prey eaten on the step, and the
+            # distance travelled when each one was eaten
+            i_eaten, d_travelled = self.find_prey(prey_local, u)
+            n_eaten = i_eaten[0].size
+        
+            #print self.prey['alive'][i_nearby[i_eaten]]
+            assert(self.prey['alive'][i_nearby[i_eaten]].all())
+            self.prey['alive'][i_nearby[i_eaten]] = 0
+        
+            self.prey_original['alive'][prey_local[i_eaten]['index']] = 0
+            self.output[self.outp:self.outp + n_eaten]['index'] = prey_local[i_eaten]['index']
+            self.output[self.outp:self.outp + n_eaten]['time'] = self.time + d_travelled / self.speed
+            self.outp += n_eaten
+            self.population -= n_eaten
+        
+        # Update time and predator position
+        self.time += step_length / self.speed
+        self.x_pred += u
+        
+    def find_prey(self, prey_local, u):
+        # Project onto u and u_perp by rotating candidate points. 
+        # Predator position before step is used as centre of rotation
+        step_length = np.hypot(u[0], u[1])
+        Q = 1 / step_length * np.array([[u[0], -u[1]], [u[1], u[0]]])
+    
+        Z = prey_local['position']
+        Z = np.dot(Z - self.x_pred, Q)
+        
+        # The step is now along the first axes. Look for pieces within radius r of 
+        # the line segment between (0,0) and (step_length, 0)
+        idx = ((np.linalg.norm(Z, axis=1) < self.radius) |
+               (np.linalg.norm(Z - np.array([step_length, 0]), axis=1) < self.radius) |
+               ((abs(Z[:,1]) < self.radius) & 
+                (0 <= Z[:, 0]) & (Z[:, 0] <= step_length)))
+        
+        # Define d_travelled to be when the predator is closest to the prey item on its 
+        # path through
+        d_travelled = Z[idx, 0]
+        
+        # If any values are negative, then assume they were eaten instantaneously.
+        # This should only happen on the first time step
+        d_travelled[d_travelled < 0] = 0.
+        
+        return idx.nonzero(), d_travelled
+    
+    def get_output(self):
+        t = np.sort(self.output[0:self.outp]['time'])
+        return (t, self.n_prey - np.arange(self.outp))
         
         
-        if self.plot:        
-            plt.setp(self.markers_alive, data=(self.x_prey[self.alive,0], self.x_prey[self.alive, 1]))
-            plt.setp(self.marker_pred, data=(self.x_pred[0], self.x_pred[1]))
-            self.fig.canvas.draw()
+def brownianwalk(sim, T=100, sigma=0.1):
+    nsteps = 2*T / (sigma / sim.speed)
 
-        # print self.popn, np.sum(self.alive), self.x_pred
-         
+    # The mess below creates too many steps, then truncates them to the right amount
+    # so that the simulation runs exactly to time, truncating the last step 
+    steps = sigma / np.sqrt(2) * np.random.randn(nsteps, 2)
+    t = np.cumsum(np.hypot(steps[:,0], steps[:,1]) / sim.speed)
+    i_last = np.where(t > T)[0][0]
+    steps = steps[:i_last+1,:]
+    steps[-1] = (T - t[i_last-1]) / (t[i_last] - t[i_last-1]) * steps[-1]
 
-    def find_prey(self, z, u):
-        # Form projections onto u and u_perp by rotating candidate points. The predator position used is before the step
-        l = np.linalg.norm(u)
-        Q = 1/l * np.array([[u[0], -u[1]], [u[1], u[0]]])
-        z = np.dot(z - self.x_pred, Q)
-        #plt.figure()
-        #plt.plot(z[:,0], z[:,1], 'ro')
-        #plt.show()
-
-        # check to see which ones satisfy constraints
-        idx = (np.linalg.norm(z, axis=1) < self.r) | (np.linalg.norm(z - np.array([l, 0.]), axis=1) < self.r) | \
-        ((abs(z[:,1]) < self.r) & (0 <= z[:,0]) & (z[:,0] <= l))
-        #plt.figure()
-        #plt.plot(z[idx,0], z[idx,1], 'ko')
-        #plt.show()
-        d = z[idx,0]
-        d[d < 0] = 0.
-        return idx.nonzero(), d
-        
-    def brownianwalk(self, T=100, sigma=0.1):
-        # Choose a number of steps that guarantees we'll go too far
-        n = 2*T / (sigma / self.v)
-
-        # Generate steps from this distribution
-        steps = sigma / np.sqrt(2)*np.random.randn(n, 2)
-
-        # Compute the cumulative time taken
-        t = np.cumsum(np.hypot(steps[:,0], steps[:,1]) / self.v)
-
-        # Take those steps, and truncate the last one
-        i_last = np.where(t > T)[0][0]
-        steps = steps[:i_last+1,:]
-        steps[-1] = (T - t[i_last-1]) / (t[i_last] - t[i_last-1]) * steps[-1]
-        
-        # Perform the walk
-        for u in steps:
-            self.step(u)
+    for u in steps:
+        sim.step(u)
 
 
 
 
 if __name__ == "__main__":
-    import matplotlib.pylab as plt
-
-    s = sim2d(mu=10000, r=0.05, plot=True)
-    sigma = 0.05;
-    T = 200
-
-    s.brownianwalk(T=T, sigma=sigma)
-    #s.step(array([0.2, 0.2]))    
-    
-    plt.plot(s.t_eat[0:s.pointer], s.u[0:s.pointer])
+    sim = deplete2d(density=1000, radius=0.01)
+    brownianwalk(sim, T=200, sigma=0.01)
+    t, pop = sim.get_output()
+    plt.plot(t, pop)
     plt.show()
-
-
-
 
